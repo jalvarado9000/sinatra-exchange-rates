0d88b3c87f69fb8aa35c3c85ec56ee6aa29d85b0
Juan Alvarado Caraballo <104938751+jalvarado9000@users.noreply.github.com>
2023-08-20 22:51:20 +0000

    missing dynamicaly change to other endpoints

--- app.rb
+++ app.rb
@@ -5,17 +5,30 @@ require "http"
 require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:choose1)
 end
 
-=begin
-rupee = "rupee"
-pp "What do you want to convert?"
-userLocation = gets.chomp
-=end
+
+#check on how to capture the response for the exchange
+get("/AED") do
+  ex = "AED"
+  pp exchange(ex)
+ erb(:choose2)
+end
+
+#missing go back feature
+get("/AED/AFN") do
+
+  @from = "AED"
+  @to = "AFN"
+  arr = exchangeFromTo(@from,@to)
+  @current = arr[0]
+  @exchange = arr[1]
+  erb(:result)
+end
+
+
+def exchange(ex)
 exachange_api = "https://api.exchangerate.host/symbols"
 resp = HTTP.get(exachange_api)
 
@@ -24,4 +37,22 @@ raw_response = resp.to_s
 parsed_response = JSON.parse(raw_response)
 
 #Start to play with api
-pp parsed_response.fetch("symbols").fetch("AED")
+return parsed_response.fetch("symbols").fetch(ex).fetch("code")
+
+end
+
+def exchangeFromTo(from, to)
+require 'net/http'
+require 'json'
+
+url = "https://api.exchangerate.host/convert?from=" + @from + "&to=" + @to
+uri = URI(url)
+response = Net::HTTP.get(uri)
+response_obj = JSON.parse(response)
+pp queryFrom = response_obj.fetch("query").fetch("amount")
+pp infoRate = response_obj.fetch("info").fetch("rate")
+my_array = []
+my_array.push(queryFrom.to_s)
+my_array.push(infoRate.to_s)
+return my_array
+end
