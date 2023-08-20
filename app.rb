856b73aef5a89c1c37bec1e23823120bba8393b2
Juan Alvarado Caraballo <104938751+jalvarado9000@users.noreply.github.com>
2023-08-20 06:48:05 +0000

    got response dowm

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "json"
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
 end
+
+=begin
+rupee = "rupee"
+pp "What do you want to convert?"
+userLocation = gets.chomp
+=end
+exachange_api = "https://api.exchangerate.host/symbols"
+resp = HTTP.get(exachange_api)
+
+raw_response = resp.to_s
+
+parsed_response = JSON.parse(raw_response)
+
+#Start to play with api
+pp parsed_response.fetch("symbols").fetch("AED")
