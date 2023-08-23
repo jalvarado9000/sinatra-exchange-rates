70a36a9ed5837bbae474de3359871337f8cc1b53
Juan Alvarado Caraballo <104938751+jalvarado9000@users.noreply.github.com>
2023-08-23 02:41:40 +0000

    finished but failed one test

--- app.rb
+++ app.rb
@@ -4,27 +4,20 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
+def exchange
+  exachange_api = "https://api.exchangerate.host/symbols"
+  resp = HTTP.get(exachange_api)
+  
+  raw_response = resp.to_s
+  
+  parsed_response = JSON.parse(raw_response)
+  
+  #Start to play with api
+  return parsed_response
+  
+  end
 
-
-$currencyList = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN",
-"BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL",
-"BSD","BTC","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF",
-"CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF",
-"DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP",
-"GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL",
-"HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK",
-"JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW",
-"KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD",
-"MDL","MGA","MKD","MMK","MNT","MOP","MRO","MRU","MUR","MVR",
-"MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD",
-"OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON",
-"RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP",
-"SLL","SOS","SRD","SSP","STD","STN","SVC","SYP","SZL","THB",
-"TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX",
-"USD","UYU","UZS","VEF","VES","VND","VUV","WST","XAF","XAG",
-"XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW",
-"ZWL"]
-
+$currencyList = exchange
 
 get("/") do
   @generate = $currencyList
@@ -38,7 +31,7 @@ get("/:currency") do
   @generate = $currencyList
   ex = params[:currency].to_s
   @curr = ex
-  exchange(ex)
+  @hash = exchange
  erb(:choose2)
 end
 
@@ -54,18 +47,7 @@ get("/:currency/:exchange") do
 end
 
 
-def exchange(ex)
-exachange_api = "https://api.exchangerate.host/symbols"
-resp = HTTP.get(exachange_api)
 
-raw_response = resp.to_s
-
-parsed_response = JSON.parse(raw_response)
-
-#Start to play with api
-return parsed_response.fetch("symbols").fetch(ex).fetch("code")
-
-end
 
 def exchangeFromTo(from, to)
 require 'net/http'
@@ -75,8 +57,8 @@ url = "https://api.exchangerate.host/convert?from=" + @from + "&to=" + @to
 uri = URI(url)
 response = Net::HTTP.get(uri)
 response_obj = JSON.parse(response)
-pp queryFrom = response_obj.fetch("query").fetch("amount")
-pp infoRate = response_obj.fetch("info").fetch("rate")
+queryFrom = response_obj.fetch("query").fetch("amount")
+infoRate = response_obj.fetch("info").fetch("rate")
 my_array = []
 my_array.push(queryFrom.to_s)
 my_array.push(infoRate.to_s)
