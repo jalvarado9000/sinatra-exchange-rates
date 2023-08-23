e45c6000dfb79498e1e22dfd2ff495d857b298a2
Juan Alvarado Caraballo <104938751+jalvarado9000@users.noreply.github.com>
2023-08-23 01:32:04 +0000

    finished dont know why my code is not passing.

--- app.rb
+++ app.rb
@@ -4,23 +4,49 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
+
+
+$currencyList = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN",
+"BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL",
+"BSD","BTC","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF",
+"CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF",
+"DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP",
+"GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL",
+"HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK",
+"JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW",
+"KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD",
+"MDL","MGA","MKD","MMK","MNT","MOP","MRO","MRU","MUR","MVR",
+"MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD",
+"OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON",
+"RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP",
+"SLL","SOS","SRD","SSP","STD","STN","SVC","SYP","SZL","THB",
+"TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX",
+"USD","UYU","UZS","VEF","VES","VND","VUV","WST","XAF","XAG",
+"XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW",
+"ZWL"]
+
+
 get("/") do
+  @generate = $currencyList
  erb(:choose1)
 end
 
 
 #check on how to capture the response for the exchange
-get("/AED") do
-  ex = "AED"
-  pp exchange(ex)
+get("/:currency") do
+ 
+  @generate = $currencyList
+  ex = params[:currency].to_s
+  @curr = ex
+  exchange(ex)
  erb(:choose2)
 end
 
 #missing go back feature
-get("/AED/AFN") do
+get("/:currency/:exchange") do
 
-  @from = "AED"
-  @to = "AFN"
+  @from = params[:currency].to_s
+  @to = params[:exchange].to_s
   arr = exchangeFromTo(@from,@to)
   @current = arr[0]
   @exchange = arr[1]
